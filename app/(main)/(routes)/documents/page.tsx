41a710e471513e2ea3699a55c35cdcce8ae06d89
Nikhil Kadam <nickkadam23@gmail.com>
2023-10-26 14:04:25 +0530

    documents page setup

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+"use client";
 
-type Props = {};
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle, PlusCircleIcon } from "lucide-react";
+import Image from "next/image";
 
-function page({}: Props) {
-  return <div>page</div>;
+function page() {
+  const { user } = useUser();
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center space-y-4">
+      <Image
+        src={"/empty.png"}
+        width={300}
+        height={300}
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src={"/empty-dark.png"}
+        width={300}
+        height={300}
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Quickflow
+      </h2>
+      <Button>
+        <PlusCircle className="mr-2 h-4 w-4" /> Create a note
+      </Button>
+    </div>
+  );
 }
 
 export default page;
