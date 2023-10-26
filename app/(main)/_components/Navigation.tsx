5704b3df0dc544e98947d06eb648faf92cc37aec
Nikhil Kadam <nickkadam23@gmail.com>
2023-10-26 14:48:34 +0530

    useritem dropdown in sidebar

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -3,6 +3,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import UserItem from "./UserItem";
 
 function Navigation() {
   const pathname = usePathname();
@@ -109,7 +110,7 @@ function Navigation() {
           <ChevronsLeft />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
