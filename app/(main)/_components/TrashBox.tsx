32216ee31d6f5e4f896e71745970f8bcd64cd837
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-07-21 23:05:33 +0530

    fix: show trash & recovery options on hover

--- app/(main)/_components/TrashBox.tsx
+++ app/(main)/_components/TrashBox.tsx
@@ -20,6 +20,7 @@ export const TrashBox = () => {
   const remove = useMutation(api.documents.remove);
 
   const [search, setSearch] = useState("");
+  const [hoveredDocumentId, setHoveredDocumentId] = useState<string>("");
 
   const filteredDocuments = documents?.filter((document) => {
     return document.title.toLowerCase().includes(search.toLowerCase());
@@ -87,28 +88,32 @@ export const TrashBox = () => {
             key={document._id}
             role="button"
             onClick={() => onClick(document._id)}
+            onMouseEnter={() => setHoveredDocumentId(document._id)}
+            onMouseLeave={() => setHoveredDocumentId("")}
             className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
           >
             <span className="truncate pl-2">
               {document.title}
             </span>
-            <div className="flex items-center">
-              <div
-                onClick={(e) => onRestore(e, document._id)}
-                role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
-              >
-                <Undo className="h-4 w-4 text-muted-foreground" />
-              </div>
-              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+            {hoveredDocumentId === document._id && (
+                <div className="flex items-center">
                 <div
+                  onClick={(e) => onRestore(e, document._id)}
                   role="button"
                   className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
-                  <Trash className="h-4 w-4 text-muted-foreground" />
+                  <Undo className="h-4 w-4 text-muted-foreground" />
                 </div>
-              </ConfirmModal>
-            </div>
+                <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                  <div
+                    role="button"
+                    className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
+                  >
+                    <Trash className="h-4 w-4 text-muted-foreground" color="red"/>
+                  </div>
+                </ConfirmModal>
+              </div>
+            )}
           </div>
         ))}
       </div>
